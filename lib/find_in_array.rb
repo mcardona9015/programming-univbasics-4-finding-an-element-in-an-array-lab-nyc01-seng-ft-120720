bdce61121ad9d1b8340e250ecd9b55dcffb5a01a
Michael Cardona <mcardona9015@gmail.com>
2020-11-22 17:13:23 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  count = 0
+  for index in array
+    if index == value_to_find
+      return count
+    else
+      count += 1
+    nil
 end
